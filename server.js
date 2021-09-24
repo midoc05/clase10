f18dd7727b93dd1f85d8ffece05a097772c37580
midoc05 <dominique.croce@gmail.com>
2021-09-23 23:35:25 -0300

    link public agregado

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const server = express(); 
 const path = require("path"); 
 
+server.use("/static", express.static("/public"));
+
 server.get ('/', (req,res) => {
 res.send('index')
 })
